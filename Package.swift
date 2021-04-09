524bcffa8827023080214aa48505197c6ad43813
Bartek <bartus000@gmail.com>
2021-04-09 13:34:14 +0200

    fix package.swift

--- Package.swift
+++ Package.swift
@@ -4,8 +4,12 @@
 import PackageDescription
 
 let package = Package(
-  name: "Web3swift",
-  products: [
+    name: "Web3swift",
+    platforms: [
+        .iOS(SupportedPlatform.IOSVersion.v9)
+    ],
+    
+    products: [
     // Products define the executables and libraries produced by a package, and make them visible to other packages.
     .library(name: "web3swift", targets: ["web3swift"]),
     ],
